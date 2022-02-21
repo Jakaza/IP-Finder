ba4baba47536f2c9bc3cac6ddc503b4c7ddddd71
Themba <69904835+Jakaza@users.noreply.github.com>
2022-02-21 19:27:40 +0200

    Create search location by IP address
    Creating searching engine and add google map on the web

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # IP-Finder
-ip-geolocation-342017
-AIzaSyAtUpCNzUtUl0W-35BbzTrMdnTMr2pkLQk
--- app.js
+++ app.js
@@ -1,32 +1,52 @@
 const loaderEl = document.getElementById('loader-container');
 const searchButton = document.getElementById('search-addon');
-const inputEl = document.querySelector('.inputEl');
+const inputEl = document.getElementById('inputEl');
+
 document.addEventListener('DOMContentLoaded', loadData)
+document.addEventListener('click', fetchData)
 
 function loadData() {
 
     let http = new XMLHttpRequest();
-    http.onload = function () {
+    http.onload = function() {
         if (this.status == 200) {
             let data = JSON.parse(http.response);
             displayData(data);
         }
     }
-    http.onprogress = function () {
+    http.onprogress = function() {
         loaderEl.style.display = 'block';
     }
     http.open('GET', 'http://ip-api.com/json/')
     http.send();
 }
 
+
+async function fetchData() {
+    try {
+        const inputValue = inputEl.value;
+        let response = await fetch('http://ip-api.com/json/' + inputValue)
+        let data = await response.json();
+        displayData(data);
+
+    } catch (error) {
+        console.log("data not found");
+    }
+}
+
+
 function displayData(data) {
     loaderEl.style.display = 'none';
-    let { as, city, country, countryCode, isp, lat, lon, org, query, region, regionName, timezone, zip } = data;
 
-    document.getElementById('name').innerHTML = city;
-    document.getElementById('ipAddress').innerHTML = query;
+    console.log(data);
 
-    let template = `
+    if (data.status == "success") {
+        let { as, city, country, countryCode, isp, lat, lon, org, query, region, regionName, timezone, zip } = data;
+
+        document.getElementById('name').innerHTML = city;
+        document.getElementById('ipAddress').innerHTML = query;
+
+        let template = `
         <tbody>
             <tr>
               <th scope="row">city</th>
@@ -66,8 +86,23 @@ function displayData(data) {
             </tr>
           </tbody>
     `;
-    document.querySelector('.table').innerHTML = template;
+        document.querySelector('.table').innerHTML = template;
+    } else {
+
+        document.getElementById('name').innerHTML = "Does not Exist";
+        document.getElementById('ipAddress').innerHTML = data.query;
+
+        errTemplate = `
+        <div class="invalid-container">
+        <div class="inner-invalid-container">
+            <h3>status : ${data.status}</h3>
+            <p>${data.message}</p>
+            <div class="img-container">
+                <img src="/500944-200.png" alt="" srcset="">
+            </div>
+        </div>
+        </div>
+      `
+        document.querySelector('.table').innerHTML = errTemplate;
+    }
 }
-
-
-
