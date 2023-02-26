d2b1a41ec5281ae591f2ffdc83faed2d68db88dd
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2023-02-26 13:41:08 +0000

    Change File Structure to MVC

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const ejs = require('ejs')
+const path = require("path")
+const router = require('./routes/home')
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs')
+
+app.use(router);
+
+
+
+app.listen(3000, () => {
+  console.log('server started');
+});
