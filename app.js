44048e42b2aa4bc5fdab0691271181dbd6b0c9fc
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2023-04-20 19:26:46 +0200

    REQUEST FROM CLIENT

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
 
-const formEl = document.getElementById("formEl");
 
 
-formEl.addEventListener("submit", (el)=>{
-  el.preventDefault();
 
-  let URL = formEl['url'].value;
+alert();
 
-  console.log(URL);
 
-})
 
-const lookup = (url) =>{
-  const request = new XMLHttpRequest();
-  request.open("GET", url,true);
-  request.onload = () =>{
-    if(request.readyState == 4 && request.status == 200){
-      const data = JSON.parse(request.response)
-      console.log(data);
-    } 
-  }
-  request.onprogress = () =>{
-    console.log("Loading DATA");
-  }
-  request.send();
-}
 
-function displayData(data) {
+
+
+
+
+
+
+
+
+// const formEl = document.getElementById("formEl");
+
+
+// formEl.addEventListener("submit", (el)=>{
+//   el.preventDefault();
+
+//   let URL = formEl['url'].value;
+
+//   console.log(URL);
+
+// })
+
+// const lookup = (url) =>{
+//   const request = new XMLHttpRequest();
+//   request.open("GET", url,true);
+//   request.onload = () =>{
+//     if(request.readyState == 4 && request.status == 200){
+//       const data = JSON.parse(request.response)
+//       console.log(data);
+//     } 
+//   }
+//   request.onprogress = () =>{
+//     console.log("Loading DATA");
+//   }
+//   request.send();
+// }
+
+// function displayData(data) {
   
 }
 
